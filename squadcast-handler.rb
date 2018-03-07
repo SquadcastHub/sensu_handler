222349275b7867f5d0cb68370ad54a2f95a19295
Kaushal Jain <Kaushal@squadcast.com>
2018-03-07 18:59:15 +0530

    Squadcast Sensu Handler

---
+++ squadcast-handler.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+require 'json'
+require 'net/http'
+require 'net/https'
+require 'uri'
+
+
+url_hook = "https://api.squadcast.com/v1/incidents/sensu/"+ARGV[0]
+
+event = JSON.parse(STDIN.read, :symbolize_names => true)
+
+uri = URI(url_hook)
+http = Net::HTTP.new(uri.host, uri.port)
+http.read_timeout = 10
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+req = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+req.body = event.to_json
+res = http.request(req)
+puts "Data sent to Squadcast"
